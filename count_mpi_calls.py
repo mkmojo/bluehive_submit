a4cfd7b8bae07591919230c27b4cae3e916fc651
Qiyuan Qiu <qiuqiyuan@gmail.com>
2014-08-05 20:06:12 -0400

    script for submitting tasks to bluehivew

--- count_mpi_calls.py
+++ count_mpi_calls.py
@@ -1,14 +1,18 @@
 __author__ = 'qiuqiyuan'
 
 import numpy as np
+import os
+import sys
 
 
 def main():
     # this function will parse the output file and count mpi count
-    data = np.loadtxt("2_1", dtype=int)
-    print data
-    cycle = 2 * 2
-    m = np.resize(data, (len(data) / (cycle * 2), cycle * 2))
+    for file_name in sys.argv[1:]:
+        data = np.loadtxt(file_name, dtype=int)
+        list = file_name.split("_")
+        num_proc = int( list[0] )
+        cycle = num_proc * 2
+        m = np.resize(data, (len(data) / (cycle * 2), cycle * 2))
 
     for i, row in enumerate(m):
         if i+1 < (len(data) / (cycle * 2)):
